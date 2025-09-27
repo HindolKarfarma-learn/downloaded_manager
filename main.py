aef7dcf2b0ad2a9ffb4575e3a692962a7b56185f
HindolKarfarma-learn <hindolkarfarma@gmail.com>
2025-09-28 00:54:44 +0530

    add extractExtention function this takes strings and returns the .extention if no . is present , whole string is returned multiple doted extention not considered, like .tar.gz

--- main.py
+++ main.py
@@ -7,11 +7,21 @@
 import time
 import os
 
+def extractExtention(fileName):
+    for i in range (len(fileName)-1,-1,-1) : # remember ranges stops at one element before
+        if fileName[i] == ".":
+            break
+        else:
+            continue
+
+    ext=fileName[i:]
+    return ext
+
 delay=30
 path="~/Downloads"
 downloadDir=os.path.expanduser(path)    #os.listdit cant recognise ~/   && we will not know or need to know username
 while True:
     containts=os.listdir(downloadDir)
     for i in containts:
-        print(i)
+        print(extractExtention(i))
     time.sleep(delay)
