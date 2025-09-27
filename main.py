ff53fe415ba90f73512d61757dfeb6a9a6febcbd
HindolKarfarma-learn <hindolkarfarma@gmail.com>
2025-09-27 17:54:36 +0530

    add main.py
    add main.py and add the algotrithm in words in main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#   a loop runs
+#       stores the file name and extention in a list
+#       loop in the list
+#           detects the extention
+#           moves the file to designated directory according to the extention
+#       wait for t secounds
