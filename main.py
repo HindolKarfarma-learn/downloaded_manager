f36b75bf33c1cd98e1234b5021af4724205ff8b7
HindolKarfarma-learn <hindolkarfarma@gmail.com>
2025-09-27 18:22:38 +0530

    add master loop
    delay is the delay after which loop iterates again

--- main.py
+++ main.py
@@ -4,3 +4,9 @@
 #           detects the extention
 #           moves the file to designated directory according to the extention
 #       wait for t secounds
+import time
+
+delay=3
+while True:
+    print ("hello world")
+    time.sleep(delay)
